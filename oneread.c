72922381a39225a7aba83e27255d478283cecb2a
Richard Durbin <rd109@cam.ac.uk>
2025-08-14 01:47:38 +0100

    simplify nName == 1

--- oneread.c
+++ oneread.c
@@ -5,7 +5,7 @@
  * Description: onebam functionality involving .1read files
  * Exported functions:
  * HISTORY:
- * Last edited: Aug 14 00:47 2025 (rd109)
+ * Last edited: Aug 14 01:46 2025 (rd109)
  * Created: Wed Aug 13 14:10:49 2025 (rd109)
  *-------------------------------------------------------------------
  */
@@ -36,9 +36,20 @@ bool yieldTx (int t, void *arg, I32 *v) // here arg is an array of OneFile
   else return false ;
 }
 
-static inline void transferLine (OneFile *ofOut, OneFile *ofIn)
-{ memcpy (ofOut->field, ofIn->field, ofIn->info[ofIn->lineType]->nField*sizeof(OneField)) ;
-  oneWriteLine (ofOut, ofIn->lineType, oneLen(ofIn), oneString(ofIn)) ;
+void loadSequence (Input *in)
+{
+  if (in->of->lineType != 'S')
+    die ("line type %c instead of expected S in %s line %lld",
+	 in->of->lineType, in->fileName, in->of->line) ;
+  in->seqLen = oneLen(in->of) ;
+  in->dna = oneDNA2bit(in->of) ;
+  while (oneReadLine (in->of) && (in->of->lineType != 'J')) ; // really should be next line...
+  if (!in->of->lineType) die ("no J line found in %s", in->fileName) ;
+  in->lcp = oneInt(in->of, 0) ;
+  if (in->lcp + oneLen(in->of) > 1024)
+    die ("read name length %d > 1024 file %s line %d",
+	 in->lcp + oneLen(in->of), in->fileName, in->of->line) ;
+  strcpy (in->name + in->lcp, oneString(in->of)) ;
 }
 
 bool merge1read (char *outfile, int nIn, char **infiles)
@@ -65,21 +76,9 @@ bool merge1read (char *outfile, int nIn, char **infiles)
 				   "D M 2 3 INT 6 STRING\nD T 3 3 INT 3 INT 3 INT\n"))
 	die ("schema mismatch for input file %s", in->fileName) ;
       if (oneGoto (in->of, 'S', 1) && oneReadLine(in->of)) // oneReadLine required to read S line
-	{ ++nSin ;
-	  in->seqLen = oneLen(in->of) ;
-	  in->dna = oneDNA2bit(in->of) ;
-	  while ((t = oneReadLine(in->of)) && (t != 'J')) ; // really should be next line...
-	  if (!t) die ("no J line found in first read of %s", in->fileName) ;
-	  if (oneInt(in->of,0) != 0)
-	    die ("non-zero LCP %d on first read of %s", oneInt(in->of,0), in->fileName) ;
-	  in->lcp = 0 ;
-	  if (oneLen(in->of) > 1024)
-	    die ("first read name length %d > 1024 file %s", oneLen(in->of), in->fileName) ;
-	  strcpy (in->name, oneString(in->of)) ;
-	}
+	{ loadSequence (in) ; ++nSin ; }
       else
 	{ warn ("input .1read file %s appears empty", in->fileName) ; // it could actually be empty
-	  oneFileClose (in->of) ;
 	  in->of = 0 ;
 	}
     }
@@ -97,71 +96,68 @@ bool merge1read (char *outfile, int nIn, char **infiles)
 
       // first write the sequence record 'S'
       oneWriteLineDNA2bit (ofOut, 'S', in->seqLen, in->dna) ; // write the sequence record
-      char *p = lastOutName, *q = in->name ; while (*p && *p == *q) { ++p ; ++q ; }
 
       // and the name in lcp/suffix form 'J'
+      char *p = lastOutName, *q = in->name ; while (*p && *p == *q) { ++p ; ++q ; }
       oneInt(ofOut,0) = (I64)(q - in->name) ;
       oneWriteLine (ofOut, 'J', strlen(q), q) ;
       strcpy (p, q) ; // copy the distinct suffix onto the shared prefix to update lastOutName
-      
-      // next copy all lines up to M, which should be the same across all inputs
-      while ((t = oneReadLine(in->of)) && t != 'M')  // there should be an M line for each object
-	transferLine (ofOut, in->of) ; // copy line
-      if (t != 'M') die ("failed to find M line on input %s", inputs[nameList[0]].fileName) ;
-      for (i = 1 ; i < nName ; ++i)
-	{ txOf[i] = inputs[nameList[i]].of ; // will need this later, and it is more direct
-	  while ((t = oneReadLine(txOf[i]) && t != 'M')) ; // read these lines also
-	  if (t != 'M') die ("failed to find M line on input %s", inputs[nameList[i]].fileName) ;
+
+      if (nName == 1) // all very simple - go to next S line
+	{ while ((t = oneReadLine(in->of)) && t != 'S')
+	    transferLine (ofOut, in->of) ; // copy line
+	  if (t) { loadSequence (in) ; ++nSin ; }
+	  else in->of = 0 ; // termination index for this input for readMerge
 	}
-
-      // now find the highest scoring M line and write it out
-      I64 mMax = oneInt(in->of,0) ;
-      OneFile *ofM = in->of ;
-      for (i = 1 ; i < nName ; ++i)
-	if (oneInt(txOf[i],0) > mMax) { ofM = txOf[i] ; mMax = oneInt(ofM,0) ; }
-      transferLine (ofOut, ofM) ; // copy the highest scoring M line
-
-      // move each txOf[] to the next line, which should be the first T line, and set up txMerge
-      for (i = 0 ; i < nName ; ++i)
-	if (oneReadLine(txOf[i]) == 'T') ++nTin ;
-	else die ("missing T line after M line input %s line %lld",
-		  inputs[nameList[i]].fileName, (long long)txOf[i]->line) ;
-      mergeRecreate (txMerge, nName, txOf) ;
-
-      // and merge the taxids
-      while ((nTx = mergeNext (txMerge, &txList)))
-	{ oneInt(ofOut,0) = oneInt(txOf[0],0) ; // copy the taxid
-	  oneInt(ofOut,1) = oneInt(txOf[0],1) ; // take the max of the best scores
-	  oneInt(ofOut,2) = oneInt(txOf[0],2) ; // sum the counts
-	  for (i = 1 ; i < nTx ; ++i)
-	    { if (oneInt(txOf[txList[i]],1) > oneInt(ofOut,1))
-		oneInt(ofOut,1) = oneInt(txOf[txList[i]],1) ;
-	      oneInt(ofOut,2) += oneInt(txOf[txList[i]],2) ;
+      else
+	{ // first copy all lines up to M, which should be the same across all inputs
+	  while ((t = oneReadLine(in->of)) && t != 'M')  // there should be an M line for each object
+	    transferLine (ofOut, in->of) ; // copy line
+	  if (t != 'M') die ("failed to find M line on input %s", inputs[nameList[0]].fileName) ;
+	  for (i = 1 ; i < nName ; ++i)
+	    { txOf[i] = inputs[nameList[i]].of ; // will need this later, and it is more direct
+	      while ((t = oneReadLine(txOf[i]) && t != 'M')) ; // read these lines also
+	      if (t != 'M') die ("failed to find M line on input %s", inputs[nameList[i]].fileName) ;
 	    }
-	  oneWriteLine (ofOut, 'T', 0, 0) ;
-	  // now advance each line, and set txOf[] to 0 if not 'T'
-	  for (i = 0 ; i < nTx ; ++i)
-	    if (oneReadLine (txOf[i]) == 'T') ++nTin ;
-	    else txOf[i] = 0 ; // terminator for the txMerge
-	}
+	  txOf[0] = in->of ;
+	  
+	  // now find the highest scoring M line and write it out
+	  I64 mMax = oneInt(in->of,0) ;
+	  OneFile *ofM = in->of ;
+	  for (i = 1 ; i < nName ; ++i)
+	    if (oneInt(txOf[i],0) > mMax) { ofM = txOf[i] ; mMax = oneInt(ofM,0) ; }
+	  transferLine (ofOut, ofM) ; // copy the highest scoring M line
+	  
+	  // move each txOf[] to the next line, which should be the first T line, and set up txMerge
+	  for (i = 0 ; i < nName ; ++i)
+	    if (oneReadLine(txOf[i]) == 'T') ++nTin ;
+	    else die ("missing T line after M line input %s line %lld",
+		      inputs[nameList[i]].fileName, (long long)txOf[i]->line) ;
+	  mergeRecreate (txMerge, nName, txOf) ;
 
-      // finally update the sequence and name in the inputs[] for the nameList 
-      for (i = 0 ; i < nName ; ++i)
-	{ Input *in = &inputs[nameList[i]] ;
-	  if (!in->of->lineType) in->of = 0 ; // terminator for the txMerge
-	  else if (in->of->lineType != 'S')
-	    die ("line type %c instead of expected S in %s line %lld",
-		 in->of->lineType, in->fileName, in->of->line) ;
-	  ++nSin ;
-	  in->seqLen = oneLen(in->of) ;
-	  in->dna = oneDNA2bit(in->of) ;
-	  while ((t = oneReadLine(in->of)) && (t != 'J')) ; // really should be next line...
-	  if (!t) die ("no J line found in %s", in->fileName) ;
-	  in->lcp = oneInt(in->of, 0) ;
-	  if (in->lcp + oneLen(in->of) > 1024)
-	    die ("read name length %d > 1024 file %s line %d",
-		 in->lcp + oneLen(in->of), in->fileName, in->of->line) ;
-	  strcpy (in->name + in->lcp, oneString(in->of)) ;
+	  // and merge the taxids
+	  while ((nTx = mergeNext (txMerge, &txList)))
+	    { oneInt(ofOut,0) = oneInt(txOf[0],0) ; // copy the taxid
+	      oneInt(ofOut,1) = oneInt(txOf[0],1) ; // take the max of the best scores
+	      oneInt(ofOut,2) = oneInt(txOf[0],2) ; // sum the counts
+	      for (i = 1 ; i < nTx ; ++i)
+		{ if (oneInt(txOf[txList[i]],1) > oneInt(ofOut,1))
+		    oneInt(ofOut,1) = oneInt(txOf[txList[i]],1) ;
+		  oneInt(ofOut,2) += oneInt(txOf[txList[i]],2) ;
+		}
+	      oneWriteLine (ofOut, 'T', 0, 0) ;
+	      // now advance each line, and set txOf[] to 0 if not 'T'
+	      for (i = 0 ; i < nTx ; ++i)
+		if (oneReadLine (txOf[i]) == 'T') ++nTin ;
+		else txOf[i] = 0 ; // terminator for the txMerge
+	    }
+
+	  // finally update the sequence and name in the inputs[] for the nameList 
+	  for (i = 0 ; i < nName ; ++i)
+	    { Input *in = &inputs[nameList[i]] ;
+	      if (in->of->lineType)	{ loadSequence (in) ; ++nSin ; }
+	      else in->of = 0 ; // terminator for the nameMerge
+	    }
 	}
     }
 
